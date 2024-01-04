ac1293200c309f6f42418435b69e1af122b0c6ca
Taiwo Joel <oluwatofunmijoel765@gmail.com>
2024-01-05 00:32:43 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: 'rgb(52,114,67)',
+        primary2: 'rgb(25,60,43)',
+        primary3: 'rgb(219,196,173)',
+      }
+    },
+  },
+  plugins: [],
+}
+
